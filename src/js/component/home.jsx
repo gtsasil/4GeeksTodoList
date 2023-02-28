ea1acb96f2ee4206bdc07287c12db470755acee8
Gleisson Santos Silva <gtsasil@gmail.com>
2023-02-28 17:44:43 +0000

    TodoList Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { Trash } from "phosphor-react"
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
+    return (
+        <div className="container">
+            <h1>My Todo List</h1>
+            <ul>
+                <li>
+                    <input
+                        type="text"
+                        onChange={(e) => { setInputValue(e.target.value) }}
+                        value={inputValue}
+                        onKeyPress={(e) => {
+                            if (e.key === "Enter") {
+                                setTodos(todos.concat(inputValue));
+                                setInputValue("");
+                            }
+                        }}
+                        placeholder="Add your item here" />
+                </li>
+                {todos.map((item, index) =>
+                    <li>
+                        {item}
+                        <button
+                            className="trash"
+                            onClick={() => setTodos(
+                                todos.filter(
+                                    (t, currentIndex) => index != currentIndex
+                                ))}>
+                            <Trash size={24} />
+                        </button>
+                    </li>
+                )}
+            </ul>
+            <div>x Task</div>
+        </div>
+    );
 };
 
 export default Home;
